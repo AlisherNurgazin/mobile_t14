b1b44281ec0f68b9f4e3043f4d3ea5220f73abef
AlisherNurgazin <71005736+AlisherNurgazin@users.noreply.github.com>
2021-05-05 02:54:24 +0600

    Add files via upload

---
+++ buses.dart
@@ -0,0 +1,21 @@
+class Buses {
+  String name;
+  String from;
+  String to;
+  DateTime fromDateTime;
+  DateTime toDateTime;
+  String seats;
+  String carNumber;
+  String icon;
+
+  Buses({
+    this.name,
+    this.from,
+    this.to,
+    this.fromDateTime,
+    this.toDateTime,
+    this.seats,
+    this.carNumber,
+    this.icon,
+  });
+}
